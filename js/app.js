615d0ed485c45a95305b1632f25507da7e592a2e
Collin Sherman <collin@Collins-MacBook-Air.local>
2021-11-09 09:23:10 -0700

    working with navbar

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-//document.body.style.backgroundColor = "red";
-//test me
+function movieApiSearch(cb) {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=Venom%20&page=1&include_adult=false`)
+        .then(response => response.json())
+        .then(data => cb(data));
+}
+movieApiSearch(getApi)
+
+function getApi(data) {
+    console.log(data)
+}
