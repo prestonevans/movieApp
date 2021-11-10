dd7444d6f30d0c7b952b871b21ada29e27905649
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-10 08:38:45 -0700

    new branch

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 let movieSearchQuery = "No Time To Die"
 let pageNum = 1
+let apiKey = `00d9cfd2d63643eb08d2411d55b3a170`
 function movieSearch(cb) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearchQuery}%20&page=${pageNum}&include_adult=false`)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieSearchQuery}%20&page=${pageNum}&include_adult=false`)
         .then(response => response.json())
         .then(data => cb(data));
 }
 function trendingMovies(cb) {
-    fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=00d9cfd2d63643eb08d2411d55b3a170`)
+    fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
         .then(response => response.json())
         .then(data => cb(data));
 }
@@ -25,8 +26,39 @@ function movieGenres(cb) {
         .then(response => response.json())
         .then(data => cb(data));
 }
-movieSearch(getApi)
-trendingMovies(getApi)
+// movieSearch(getApi)
+// trendingMovies(getApi)
 function getApi(data) {
+    console.log(data)
+}
+trendingMovies(render)
+function render(data) {
+    let trendingHTML = `<div class="android-more-section
+    ">
+    <div class="android-section-title mdl-typography--display-1-color-contrast center">Featured Movies</div>
+    <div class="android-card-container mdl-grid">`
+    for (let i = 0; i < 4; i++) {
+        trendingHTML += `
+        
+          <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+            <div class="mdl-card__media">
+            <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
+            </div>
+            <div class="mdl-card__title">
+              <h4 class="mdl-card__title-text">${data.results[i].original_title}</h4>
+            </div>
+            <div class="mdl-card__supporting-text">
+              <span class="mdl-typography--font-light mdl-typography--subhead">${data.results[i].overview}</span>
+            </div>
+            <div class="mdl-card__actions">
+              <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
+                Make the switch
+              </a>
+            </div>
+          </div>`
+    }
+    document.getElementById("movieCards").innerHTML = trendingHTML
+    console.log(data.results[0].poster_path)
+    console.log(data.results[0].id)
     console.log(data)
 }
