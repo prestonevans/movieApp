33e1e95aff82f879b39f5c11de1f0e5cb13bd703
Collin Sherman <collin@Collins-MacBook-Air.local>
2021-11-10 09:50:08 -0700

    default image

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let movieSearchQuery = "No Time To Die"
+let movieSearchQuery = "venom"
 let pageNum = 1
 let apiKey = `00d9cfd2d63643eb08d2411d55b3a170`
 let heading = "Featured Movies"
@@ -12,21 +12,21 @@ function trendingMovies(cb) {
         .then(response => response.json())
         .then(data => cb(data));
 }
-function findByID(cb) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
-        .then(response => response.json())
-        .then(data => cb(data));
-}
-function popularMovies(cb) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
-        .then(response => response.json())
-        .then(data => cb(data));
-}
-function movieGenres(cb) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
-        .then(response => response.json())
-        .then(data => cb(data));
-}
+// function findByID(cb) {
+//     fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
+//         .then(response => response.json())
+//         .then(data => cb(data));
+// }
+// function popularMovies(cb) {
+//     fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
+//         .then(response => response.json())
+//         .then(data => cb(data));
+// }
+// function movieGenres(cb) {
+//     fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
+//         .then(response => response.json())
+//         .then(data => cb(data));
+// }
 // movieSearch(getApi)
 // trendingMovies(getApi)
 function getApi(data) {
@@ -34,16 +34,23 @@ function getApi(data) {
 }
 trendingMovies(render)
 function render(data) {
+    let imageSource = ``
     let trendingHTML = `<div class="android-more-section
     ">
     <div class="android-section-title mdl-typography--display-1-color-contrast center">${heading}</div>
     <div class="android-card-container mdl-grid">`
     for (let i = 0; i < 20; i++) {
+        if (data.results[i].poster_path === null) {
+            imageSource = 'default-movie.png'
+        }
+        else {
+            imageSource = `https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}`
+        }
         trendingHTML += `
         
           <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
             <div class="mdl-card__media">
-            <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
+            <img src="${imageSource}">
             </div>
             <div class="mdl-card__title">
               <h4 class="mdl-card__title-text">${data.results[i].original_title}</h4>
@@ -59,9 +66,6 @@ function render(data) {
           </div>`
     }
     document.getElementById("movieCards").innerHTML = trendingHTML
-    console.log(data.results[0].poster_path)
-    console.log(data.results[0].id)
-    console.log(data)
 }
 
 function searchInput() {
