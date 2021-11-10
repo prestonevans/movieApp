103ad82ae3946bbc5ab50eab70538bfa96959c3c
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-10 09:49:54 -0700

    added hearts to cards

--- js/app.js
+++ js/app.js
@@ -1,45 +1,53 @@
-let movieSearchQuery = "No Time To Die"
-let pageNum = 1
-let apiKey = `00d9cfd2d63643eb08d2411d55b3a170`
-let heading = "Featured Movies"
+let movieSearchQuery = 'No Time To Die';
+let pageNum = 1;
+let apiKey = `00d9cfd2d63643eb08d2411d55b3a170`;
+let heading = 'Featured Movies';
 function movieSearch(cb) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieSearchQuery}%20&page=${pageNum}&include_adult=false`)
-        .then(response => response.json())
-        .then(data => cb(data));
+	fetch(
+		`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieSearchQuery}%20&page=${pageNum}&include_adult=false`
+	)
+		.then((response) => response.json())
+		.then((data) => cb(data));
 }
 function trendingMovies(cb) {
-    fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
-        .then(response => response.json())
-        .then(data => cb(data));
+	fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
+		.then((response) => response.json())
+		.then((data) => cb(data));
 }
 function findByID(cb) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
-        .then(response => response.json())
-        .then(data => cb(data));
+	fetch(
+		`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`
+	)
+		.then((response) => response.json())
+		.then((data) => cb(data));
 }
 function popularMovies(cb) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
-        .then(response => response.json())
-        .then(data => cb(data));
+	fetch(
+		`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`
+	)
+		.then((response) => response.json())
+		.then((data) => cb(data));
 }
 function movieGenres(cb) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`)
-        .then(response => response.json())
-        .then(data => cb(data));
+	fetch(
+		`https://api.themoviedb.org/3/search/movie?api_key=00d9cfd2d63643eb08d2411d55b3a170&language=en-US&query=${movieSearch}%20&page=1&include_adult=false`
+	)
+		.then((response) => response.json())
+		.then((data) => cb(data));
 }
 // movieSearch(getApi)
 // trendingMovies(getApi)
 function getApi(data) {
-    console.log(data)
+	console.log(data);
 }
-trendingMovies(render)
+trendingMovies(render);
 function render(data) {
-    let trendingHTML = `<div class="android-more-section
+	let trendingHTML = `<div class="android-more-section
     ">
     <div class="android-section-title mdl-typography--display-1-color-contrast center">${heading}</div>
-    <div class="android-card-container mdl-grid">`
-    for (let i = 0; i < 20; i++) {
-        trendingHTML += `
+    <div class="android-card-container mdl-grid">`;
+	for (let i = 0; i < 20; i++) {
+		trendingHTML += `
         
           <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
             <div class="mdl-card__media">
@@ -55,17 +63,18 @@ function render(data) {
               <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
                 More Details
               </a>
+              <i class="fa-solid fa-heart"></i>
             </div>
-          </div>`
-    }
-    document.getElementById("movieCards").innerHTML = trendingHTML
-    console.log(data.results[0].poster_path)
-    console.log(data.results[0].id)
-    console.log(data)
+          </div>`;
+	}
+	document.getElementById('movieCards').innerHTML = trendingHTML;
+	console.log(data.results[0].poster_path);
+	console.log(data.results[0].id);
+	console.log(data);
 }
 
 function searchInput() {
-    heading = "Search Results"
-    movieSearchQuery = document.getElementById("search-field").value.trim()
-    movieSearch(render)
+	heading = 'Search Results';
+	movieSearchQuery = document.getElementById('search-field').value.trim();
+	movieSearch(render);
 }
