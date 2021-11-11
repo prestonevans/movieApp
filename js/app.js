c8a3c1f9eab195f48456cb7122b23a67a4abfd7a
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-10 20:38:02 -0700

    made back to top button show and hide

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-let movieSearchQuery = "venom"
-let pageNum = 1
-let apiKey = `00d9cfd2d63643eb08d2411d55b3a170`
-let heading = "Featured Movies"
+let movieSearchQuery = 'venom';
+let pageNum = 1;
+let apiKey = `00d9cfd2d63643eb08d2411d55b3a170`;
+let heading = 'Featured Movies';
 function movieSearch(cb) {
 	fetch(
 		`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieSearchQuery}%20&page=${pageNum}&include_adult=false`
@@ -36,19 +36,18 @@ function getApi(data) {
 }
 trendingMovies(render);
 function render(data) {
-    let imageSource = ``
-    let trendingHTML = `<div class="android-more-section
+	let imageSource = ``;
+	let trendingHTML = `<div class="android-more-section
     ">
     <div class="android-section-title mdl-typography--display-1-color-contrast center">${heading}</div>
-    <div class="android-card-container mdl-grid">`
-    for (let i = 0; i < 20; i++) {
-        if (data.results[i].poster_path === null) {
-            imageSource = 'default-movie.png'
-        }
-        else {
-            imageSource = `https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}`
-        }
-        trendingHTML += `
+    <div class="android-card-container mdl-grid">`;
+	for (let i = 0; i < 20; i++) {
+		if (data.results[i].poster_path === null) {
+			imageSource = 'default-movie.png';
+		} else {
+			imageSource = `https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}`;
+		}
+		trendingHTML += `
           <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
             <div class="mdl-card__media">
             <img src="${imageSource}">
@@ -65,9 +64,9 @@ function render(data) {
               </a>
               <i class="fa-solid fa-heart"></i>
             </div>
-          </div>`
-    }
-    document.getElementById("movieCards").innerHTML = trendingHTML
+          </div>`;
+	}
+	document.getElementById('movieCards').innerHTML = trendingHTML;
 }
 
 function searchInput() {
@@ -75,3 +74,13 @@ function searchInput() {
 	movieSearchQuery = document.getElementById('search-field').value.trim();
 	movieSearch(render);
 }
+// show and hide back to top button
+window.onscroll = function() {
+	if (scrollY >= 1000) {
+		document.getElementById('view-source').style.visibility = 'visible';
+		document.getElementById('view-source').style.opacity = 1;
+	} else {
+		document.getElementById('view-source').style.visibility = 'hidden';
+		document.getElementById('view-source').style.opacity = 0;
+	}
+};
