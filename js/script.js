c75bea9fce47572438f5b74888cd5ebf28595f21
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-12 09:55:10 -0700

    working on endless scroll

--- js/script.js
+++ js/script.js
@@ -1,54 +1,52 @@
-
 let nameInput = document.getElementById('search-field');
 nameInput.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) {
-    searchInput();
-  }
+	if (e.keyCode === 13) {
+		searchInput();
+	}
 });
 trendingMovies(render);
 
 function searchInput() {
-  currentRender = `search`
-  heading = 'Search Results';
-  trendingHTML = `<div class="android-more-section
+	currentRender = `search`;
+	heading = 'Search Results';
+	trendingHTML = `<div class="android-more-section
   ">
   <div class="android-section-title mdl-typography--display-1-color-contrast center">${heading}</div>
-  <div class="android-card-container mdl-grid">`
-  pageNum = 1
-  movieSearchQuery = document.getElementById('search-field').value.trim();
-  movieSearch(render);
+  <div class="android-card-container mdl-grid">`;
+	pageNum = 1;
+	movieSearchQuery = document.getElementById('search-field').value.trim();
+	movieSearch(render);
 }
 function saveMovie(id) {
-  if (savedMovies.length == 0) {
-    savedMovies.push(id)
-  }
-  else {
-    for (let movie of savedMovies) {
-      if (movie == id) {
-        return
-      }
-    }
-    savedMovies.push(id)
-  }
-  console.log(savedMovies)
+	if (savedMovies.length == 0) {
+		savedMovies.push(id);
+	} else {
+		for (let movie of savedMovies) {
+			if (movie == id) {
+				return;
+			}
+		}
+		savedMovies.push(id);
+	}
+	console.log(savedMovies);
 }
 // show and hide back to top button
-window.onscroll = function () {
-  if (scrollY >= 1000) {
-    document.getElementById('view-source').style.visibility = 'visible';
-    document.getElementById('view-source').style.opacity = 1;
-  } else {
-    document.getElementById('view-source').style.visibility = 'hidden';
-    document.getElementById('view-source').style.opacity = 0;
-  }
+window.onscroll = function() {
+	if (scrollY >= 1000) {
+		document.getElementById('view-source').style.visibility = 'visible';
+		document.getElementById('view-source').style.opacity = 1;
+	} else {
+		document.getElementById('view-source').style.visibility = 'hidden';
+		document.getElementById('view-source').style.opacity = 0;
+	}
 };
-
-$(function () {
-  let $win = $(window);
-  $win.scroll(function () {
-    if ($win.height() + $win.scrollTop()
-      == $(document).height()) {
-      loadMore()
-    }
-  });
-});
+if (endlessScroll) {
+	$(function() {
+		let $win = $(window);
+		$win.scroll(function() {
+			if ($win.height() + $win.scrollTop() == $(document).height()) {
+				loadMore();
+			}
+		});
+	});
+}
