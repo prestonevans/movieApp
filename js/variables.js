00838b82623f3243f8dd4e7b52728d4bef2fd168
Collin Sherman <collin@Collins-MacBook-Air.local>
2021-11-18 08:04:53 -0700

    fixed search

--- js/variables.js
+++ js/variables.js
@@ -6,7 +6,7 @@ if (existingSaved) {
     savedMovies = existingSaved
 }
 
-let movieSearchQuery = 'venom';
+let movieSearchQuery = '';
 let pageNum = 1;
 let apiKey = `00d9cfd2d63643eb08d2411d55b3a170`;
 let heading = 'Featured Movies';
